245008bbe7d0435160ec68cf5aa1244e13a50d17
Hongbin Miao <zslrmhb@gmail.com>
2025-03-22 21:08:56 -0700

    new stuff

--- main.py
+++ main.py
@@ -2,18 +2,13 @@ from nltk.tokenize import word_tokenize
 from transformers import AutoTokenizer, AutoModel
 import torch
 
-with open("exampleText.txt", "r") as f:
-    text = f.read()
 
-print(word_tokenize(text))
+from src.utils import text_input
 
-tokenizer = AutoTokenizer.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
-model = AutoModel.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
+# with open("exampleText.txt", "r") as f:
+#     text = f.read()
 
-text = text
-inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True)
-outputs = model(**inputs)
+# print(word_tokenize(text))
 
-# Mean pooling to get sentence embeddings
-embeddings = outputs.last_hidden_state.mean(dim=1).detach().numpy()
-print(embeddings)
+
+text_input.text_input_function("Something")
